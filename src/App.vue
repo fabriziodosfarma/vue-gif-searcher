979911c2a9fc313bf078721a822498ef18a9e5cd
David Pérez López <dvpl88@gmail.com>
2020-10-10 18:28:11 +0200

    añadidas reacciones, emojis y sdk

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,83 @@
 <template src='./templates/App.html'></template>
 
 <script>
-import GifGrid from './components/GifGrid.vue'
-import Navbar from './components/Navbar.vue'
-import BannerCTA from './components/BannerCTA.vue'
-import Footer from './components/Footer.vue'
+import { GiphyFetch } from "@giphy/js-fetch-api";
+import GifGrid from "./components/GifGrid.vue";
+import Navbar from "./components/Navbar.vue";
+import BannerCTA from "./components/BannerCTA.vue";
+import Footer from "./components/Footer.vue";
+
+const gf = new GiphyFetch("zjabJCYz2uiqqJjvzeSLXSbg6E49Dr5u");
 
 export default {
-  name: 'App',
-  components: {GifGrid,Navbar,BannerCTA,Footer},
-  data: function() {
+  name: "App",
+  components: { GifGrid, Navbar, BannerCTA, Footer },
+
+  data: function () {
     return {
-      trendingGifs: null,
-      searchedGifs: null,
-      baseURL: 'https://api.giphy.com/v1/gifs/',
-      apiKey: 'zjabJCYz2uiqqJjvzeSLXSbg6E49Dr5u',
-      query: ''
-    }
+      trendingGifs: [],
+      searchedGifs: [],
+      reactionsGifs: [],
+      emojis: [],
+      query: "",
+    };
   },
   methods: {
-    fetchTrendingGifs: function() {
-      const url = `${this.baseURL}trending?api_key=${this.apiKey}&limit=8`;
-        fetch(url)
-          .then(response => response.json())
-          .then(data => this.trendingGifs = data.data)
+    async fetchTrending() {
+      try {
+        const result = await gf.trending({ limit: 8 });
+        this.trendingGifs = result.data;
+      } catch (error) {
+        console.error(`trending`, error);
+      }
     },
-    searchGifs: function() {
-      const url = `${this.baseURL}search?api_key=${this.apiKey}&q=${this.query}&limit=8`
-      fetch(url)
-        .then(response => response.json())
-        .then(data => this.searchedGifs = data.data)
-    }
+    async fetchReactions() {
+      try {
+        const result = await gf.gifs("reactions");
+        result.data.forEach((gif, index) => {
+          if (index > 7) return;
+          this.reactionsGifs.push(gif);
+        });
+      } catch (error) {
+        console.error(`reactions`, error);
+      }
+    },
+    async fetchEmojis() {
+      try {
+        const result = await gf.emoji({ limit: 8 });
+        result.data.forEach((emoji, index) => {
+          if (index > 7) return;
+          this.emojis.push(emoji);
+        });
+      } catch (error) {
+        console.error(`emojis`, error);
+      }
+    },
+    async searchGifs() {
+      this.searchedGifs = [];
+      try {
+        const result = await gf.search(`"${this.query}"`, {
+          sort: "relevant",
+          lang: "es",
+          limit: 8,
+          type: "stickers",
+        });
+        result.data.forEach((gif, index) => {
+          if (index > 7) return;
+          this.searchedGifs.push(gif);
+        });
+      } catch (error) {
+        console.error(`search`, error);
+      }
+    },
   },
-  created: function() {
-    this.fetchTrendingGifs()
-  }
-} 
+
+  created: function () {
+    this.fetchTrending();
+    this.fetchReactions();
+    this.fetchEmojis();
+  },
+};
 </script>
 
 <style>
